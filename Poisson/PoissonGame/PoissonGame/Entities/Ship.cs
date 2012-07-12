3edafd508682eed189b188fcca7717950d7ff0de
David Yu <lewen.yu@gmail.com>
2012-07-12 17:56:18 -0200

    bad hook

--- Poisson/PoissonGame/PoissonGame/Entities/Ship.cs
+++ Poisson/PoissonGame/PoissonGame/Entities/Ship.cs
@@ -4,20 +4,21 @@ namespace Poisson.Entities
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using System.Diagnostics;
     using Microsoft.Xna.Framework.Graphics;
     using Microsoft.Xna.Framework;
     using Poisson.Utility;
 
     class Ship : Entity
     {
-        enum EHookState { UPDOWN = 0 }
+        enum EHookState { UP = 0, DOWN = 1, RETRACTED = 2 } //maybe different hook types later
         const float FRICTION = 0.99f;
+        const float BORING_HOOK_VEL = 1f;
 
         Rectangle hookRect;
         Vector2 hookPos;
         EHookState hookState;
-        
-        
+        Texture2D hookSprite;
 
         public Ship() : base()
         {
@@ -31,9 +32,11 @@ namespace Poisson.Entities
         public override void Initialise(Game game)
         {
             SpriteTexture = game.Content.Load<Texture2D>("Art/spritesheet");
+            hookSprite = game.Content.Load<Texture2D>("Art/hook");
             this.SpriteRect = new Rectangle(0, 0, 256, 164);
             this.hookRect = new Rectangle(0, 0, 40, 40);
             this.hookPos = new Vector2(this.Pos.X, this.Pos.Y);
+            this.hookState = EHookState.DOWN;
             this.Vel = new Vector2(3.0f, 0.0f);
             this.Facing = true;
         }
@@ -44,17 +47,27 @@ namespace Poisson.Entities
             this.Orient += this.AngVel;
             //this.Vel *= FRICTION;
 
-            if (this.Pos.X < 0.0f || this.Pos.X > 600) {
+            if (this.Pos.X < 0.0f || this.Pos.X > 600) { //turn around if hit edge
                 this.Vel *= -1.0f;
                 this.Facing = !this.Facing;
             }
 
             switch (this.hookState) {
-                case EHookState.UPDOWN:
+                case EHookState.UP:
+                    this.hookPos.Y -= BORING_HOOK_VEL;
+                    if (this.hookPos.Y <= 40.0f)
+                        this.hookState = EHookState.RETRACTED;
                     break;
-                default:
+                case EHookState.DOWN:
+                    this.hookPos.Y += BORING_HOOK_VEL;
+                    if (this.hookPos.Y >= 400.0f)
+                        this.hookState = EHookState.UP;
+                    break;
+                case EHookState.RETRACTED:
                     break;
             }
+
+            Debug.WriteLine(this.hookState);
         }
 
         public override void Render(GameTime gameTime, SpriteBatch batch)
@@ -69,7 +82,9 @@ namespace Poisson.Entities
             batch.Draw(this.SpriteTexture, this.Pos,
                 this.SpriteRect, Color.White,
                 this.Orient, new Vector2(0f, 0f), 1.0f, spriteEffects, 0.0f);
-                
+            batch.Draw(this.hookSprite, this.Pos+this.hookPos,
+                this.SpriteRect, Color.White,
+                this.Orient, new Vector2(0f, 0f), 1.0f, spriteEffects, 0.0f);    
         }
     }
 }
