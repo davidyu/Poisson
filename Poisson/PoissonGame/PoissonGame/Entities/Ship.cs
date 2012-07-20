717a0af9aaa674562776366912c3bed2e5a7b11b
David Yu <lewen.yu@gmail.com>
2012-07-20 04:37:06 -0200

    hook update hook now in correct position. Tedious but worthwhile...for now.

--- Poisson/PoissonGame/PoissonGame/Entities/Ship.cs
+++ Poisson/PoissonGame/PoissonGame/Entities/Ship.cs
@@ -19,8 +19,10 @@ namespace Poisson.Entities
         const float FRICTION = 0.99f;
         const float BORING_HOOK_VEL = 5f;
         const int BOTTOM_OF_SCREEN = 400;
-        const int TOP_OF_ROD = -20;
-        const int ROD_OFFSET = 100;
+
+        const int TOP_OF_ROD = 20;
+        const int ROD_OFFSET = 227;
+        const int ROD_OFFSET_FLIP = 10;
 
         private Rectangle _hookSpriteRect;
         private Rectangle _hookRect;
@@ -53,7 +55,7 @@ namespace Poisson.Entities
             this._hookSpriteRect = new Rectangle(0, 255, 16, 22);
             this.SpriteRect = new Rectangle(0, 0, 256, 164);
             this._hookRect = new Rectangle(0, 0, 40, 40);
-            this.hookPos = new Vector2(this.Pos.X + ROD_OFFSET, this.Pos.Y);
+            this.hookPos = new Vector2(ROD_OFFSET, 0);
             this.hookState = EHookState.DOWN;
             this.Vel = new Vector2(3.0f, 0.0f);
             this.Facing = true;
@@ -114,14 +116,22 @@ namespace Poisson.Entities
             SpriteEffects spriteEffects = new SpriteEffects();
 
             if (!Facing)
+            {
                 spriteEffects = SpriteEffects.FlipHorizontally;
+                hookPos = new Vector2(ROD_OFFSET_FLIP, hookPos.Y);
+            }
+            else
+            {
+                hookPos = new Vector2(ROD_OFFSET, hookPos.Y);
+            }
 
             Rectangle destRect = new Rectangle((int)this.Pos.X, (int)this.Pos.Y, (int)SpriteRect.Width, (int)SpriteRect.Height);
             //batch.Draw(this.SpriteTexture, this.Pos, Color.White);
             batch.Draw(this.SpriteTexture, this.Pos,
                 this.SpriteRect, Color.White,
                 this.Orient, new Vector2(0f, 0f), 1.0f, spriteEffects, 0.4f);
-            batch.Draw(this.hookSprite, this.Facing ? this.Pos + this.hookPos : this.Pos - this.hookPos,
+
+            batch.Draw(this.hookSprite, this.Pos + this.hookPos,
                 _hookSpriteRect, Color.White,
                 this.Orient, new Vector2(0f, 0f), 1.0f, spriteEffects, 0.3f);    
         }
